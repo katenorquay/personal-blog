fc3199471c4ab60f2babd04fae3620578f04a52f
katenorquay <katenorquay@live.com>
2018-09-07 18:43:11 +1200

    added google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
+
 module.exports = {
   siteMetadata: {
     title: 'Happiness Project',
@@ -17,10 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-        // Puts tracking script in the head instead of the body
+        trackingId: process.env.TRACKING_ID,
         head: false,
-        // Setting this parameter is also optional
         respectDNT: true,
       },
     },
