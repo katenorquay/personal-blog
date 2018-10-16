5b7fb8d25b8590f336215e293d9f24af899c85a6
katenorquay <katenorquay@live.com>
2018-10-16 22:25:16 +1300

    tweak intro text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ const HomePage = ({data}) => {
         <h1>Welcome to my blog about happiness!</h1>
         <p>
           My name's Kate. I'm a software engineer with a passion for reading, hot chips and colourful clothes and personal development.
-          Since I was 21 I've been struggling with depression. Not cripplingly so, but enough that my life was turning out to be somewhere between aggressively average and a bit of a bummer.
-          This year I got sick of it. I was struggling to feel excited, or passionate, even though I actually have a pretty great life.
+          Since I was 21 I've been struggling with depression. Not cripplingly so, but enough that I felt like I was just getting through the days, rather than really experiencing my life.
+          This year I got sick of it.
           I decided that I was going to try everything I could to make myself happier, and along the way, learn to be kinder, more energentic and more accepting.
           This blog is my record of that journey, ideas i've collected, ways of thinking and habits that work for me, and things that inspire me.
         </p>
