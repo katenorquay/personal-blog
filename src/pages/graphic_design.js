9d947fee4107dfbbbcd263fdadd5d1ec16071c4e
katenorquay <katenorquay@live.com>
2019-04-21 19:40:56 +1200

    added new files

---
+++ src/pages/graphic_design.js
@@ -0,0 +1,20 @@
+//Core Dependencies
+import React from 'react'
+
+import bannerImage from '../assets/graphic_design.png'
+
+//Styles
+import styles from './styles.module.scss'
+
+const GraphicDesignPage = ({data}) => {
+  return (
+    <div className={styles.homePage}>
+      <img className={styles.bannerImage} src={bannerImage}></img>
+      <div className={styles.infoBanner}>
+        <h1>Welcome to my blog about happiness!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default GraphicDesignPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,20 +11,7 @@ import styles from './styles.module.scss'
 
 const HomePage = ({data}) => {
   return (
-    <div className={styles.homePage}>
-      <img className={styles.bannerImage} src={bannerImage}></img>
-      <div className={styles.infoBanner}>
-        <h1>Welcome to my blog about happiness!</h1>
-        <p>
-          My name's Kate. I'm a software engineer with a passion for reading, hot chips and colourful clothes and personal development.
-          Since I was 21 I've been struggling with depression. Not cripplingly so, but enough that I felt like I was just getting through the days, rather than really experiencing my life.
-          This year I got sick of it.
-          I decided that I was going to try everything I could to make myself happier, and along the way, learn to be kinder, more energentic and more accepting.
-          This blog is my record of that journey, ideas i've collected, ways of thinking and habits that work for me, and things that inspire me.
-        </p>
-      </div>
-      <BlogGrid data={data}/>
-    </div>
+    <h1>Index page</h1>
   )
 }
 
@@ -33,7 +20,7 @@ export default HomePage
 export const query = graphql`
   query BlogQuery {
     allMarkdownRemark (
-      filter: { fileAbsolutePath: {regex : "\/blog/"} },
+      filter: { fileAbsolutePath: {regex : "\/writing/"} },
       sort: { order: DESC, fields: [frontmatter___date]},
     ) {
       totalCount
