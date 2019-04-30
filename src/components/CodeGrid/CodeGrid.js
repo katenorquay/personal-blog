fc037a203d59b5db2cdb2367093c1525a35b0fb3
katenorquay <katenorquay@live.com>
2019-04-30 22:45:05 +1200

    added code intro text

--- src/components/CodeGrid/CodeGrid.js
+++ src/components/CodeGrid/CodeGrid.js
@@ -7,6 +7,21 @@ import styles from './styles.module.scss'
 const CodeGrid = ({data}) => {
   return (
     <div className={styles.banner}>
+      <div className={styles.introText}>
+        <p>
+          I started learning to code two and a half years ago at a programming Bootcamp in Wellington, Enspiral Dev Academy.
+        </p>
+        <p>
+          Up until this point in my life I'd always thought of myself as pretty smart, as I'd never struggled at school.
+          That all changed on the first day of bootcamp. Everyday I would struggle, and go home feeling like my head was about to pop.
+          I found that my naturally creative brain loved the challenge of thinking logically. 
+          There is no feeling quite like solving a really difficult problem.
+          I knew I had found one of my passions.
+        </p>
+        <p>
+          Over the last few years I have worked on many projects, predominantly using Javascript and React. You can see examples of them below.
+        </p>
+      </div>
       <ul className={styles.list}>
         {data.allMarkdownRemark.edges.map(({ node }) => {
           return (
